26c85a6758c0e39fd92fb62a8813a05061046714
DEDALX305 <dedalx305@mail.ru>
2017-06-01 23:53:35 +0500

    V_0.29

--- DSMPM/ConsoleApplication2/ConsoleApplication2/Source.cpp
+++ DSMPM/ConsoleApplication2/ConsoleApplication2/Source.cpp
@@ -1,7 +1,7 @@
 #include <iostream> // для оператора cout
 #include <cassert>  // для функции assert
 /*==============================================*/
-#define DEBUG
+#define NDEBUG
 using namespace std;
 /*==============================================*/
 
@@ -23,7 +23,6 @@ class Memory_manager
 {
 public:
 	float Large_ratio, Small_ratio, FragMM, Number_of_fragments, SizeFragment, Ftml, GDAfrag_t1, OutFragM, SizeResultantSubfragment, SendAF, ClearFM, SizeFragmentLarge_ratio;
-	
 	Memory_manager(float Size1, float Size2, float SFastMemory, float SMainMemory, float MainMemThroughput)
 	{
 /*Константные переменные --------------------------------------------------------------*/
@@ -49,30 +48,29 @@ public:
 #ifdef DEBUG 
 	cout << "Ftml " << Ftml << endl;
 #endif
-		
+/*-------------------------------------------------------------------------------------*/
 		Number_of_fragments = Large_ratio / (((SFastMemory - Small_ratio) - Small_ratio) / 2); // Количество фрагментов.
 		assert(Number_of_fragments > 1);
 #ifdef DEBUG 
 	cout << "Number_of_fragments " << Number_of_fragments << endl;
 #endif
-			
+/*-------------------------------------------------------------------------------------*/
 		SizeFragmentLarge_ratio = ((SFastMemory - Small_ratio) - Small_ratio) / 2; // Размер фрагмента большого отношения.
 		assert(SizeFragmentLarge_ratio > NULL);
 #ifdef DEBUG 
 	cout << "SizeFragmentLarge_ratio " << SizeFragmentLarge_ratio << endl;
 #endif
-
+/*-------------------------------------------------------------------------------------*/
 		SizeResultantSubfragment = SizeFragmentLarge_ratio + Small_ratio; // Размер результирующего отношения
 		assert(SizeResultantSubfragment > NULL);
 #ifdef DEBUG 
 	cout << "SizeResultantSubfragment " << SizeResultantSubfragment << endl;
 #endif
-		
+/*-------------------------------------------------------------------------------------*/
 		OutFragM = SizeResultantSubfragment / MainMemThroughput;
 #ifdef DEBUG 
 	cout << "OutFragM " << OutFragM << endl;
-#endif
-			
+#endif			
 	}
 };
 
@@ -83,7 +81,7 @@ public:
 	
 	Manager_of_parallel_agents(float k1, float FastMemThroughputm, float PerfParallelAgentManager, float MainMemThroughput)
 	{
-/*Константные переменные --------------------------------------------------------------*/
+/*---------------------------Константные переменные-----------------------------------*/
 		t_g = 1;// Время затраченное на создание группы агентов.
 		t_f = 1; // Время проверки свободных виртуальных процессоров. 
 		GDAfrag_t2 = 1;// Время затраченное на отправку менеджером памяти адресов подфрагментов хранящихся в быстрой памяти менеджеру параллельных агентов.
@@ -105,60 +103,45 @@ int main()
 	cout << " DEBUG MODE!" << endl;
 #endif
 	
-	float	_Size1, Size1, _Size2, Size2, SFastMemory, SMainMemory, MainMemThroughput, TimeFirst, TimeFinal, t_load, Time_j, time_j, N_ProcessingSteps;
-	float	N_ProcRequest, TALL, FastMemThroughput, PerfParallelAgentManager, t_0, k1, k2, SizeCort;
-/*Константные переменные --------------------------------------------------------------*/
+	float	TimeFirst, TimeFinal, t_load, Time_j, time_j, N_ProcessingSteps, TALL, t_0, k1;
+	float	Size1, Size2, SFastMemory, SMainMemory, MainMemThroughput, FastMemThroughput, PerfParallelAgentManager;
+/* -----------------------------Константные переменные---------------------------------*/
 	TimeFinal = 1;
 	k1 = 2500000;
 /*-------------------------------------------------------------------------------------*/
-	
 	cout << "Отношение 1 \n";
-	Size1 = 10000000;
-	/*cin >> _Size1;*/
-	/*Size1 = _Size1 / ((SFastMemory / 100 ) * 38);*/
+	cin >> Size1;
 	assert(Size1 > NULL);
 	assert(Size1 != NULL);
-#ifdef DEBUG 
-		cout << "Size1 " << Size1 << endl;
-#endif
-
 	cout << "Отношение 2 \n";
-	Size2 = 1000000;
-	/*cin >> _Size2;*/
-	/*Size2 = _Size2 / ((SMainMemory / 100) * 44);*/
+	cin >> Size2;
 	assert(Size2 > NULL);
 	assert(Size2 != NULL);
-#ifdef DEBUG 
-	cout << "Size2 " << Size2 << endl;
-#endif
-
 	cout << "Введите размер быстрой памяти \n";
-	SFastMemory = 16000000;
+	cin >> SFastMemory;
 	assert(SFastMemory > NULL);
 	assert(SFastMemory != NULL);
 	cout << "Введите размер основной памяти \n";
-	SMainMemory = 39300000;
+	cin >> SMainMemory;
 	assert(SMainMemory > NULL);
 	assert(SMainMemory != NULL);
-
 	cout << "Введите пропускную способность основной памяти \n";
-	MainMemThroughput = 900000;
+	cin >> MainMemThroughput;
 	assert(MainMemThroughput > NULL);
 	assert(MainMemThroughput != NULL);
 	cout << "Введите пропускную способность быстрой памяти \n";
-	FastMemThroughput = 4000000;
+	cin >> FastMemThroughput;
 	assert(FastMemThroughput > NULL);
 	assert(FastMemThroughput != NULL);
 	cout << "Введите производительсноть менеджера паралельных агентов \n";
-	PerfParallelAgentManager = 4;
+	cin >> PerfParallelAgentManager;
 	assert(PerfParallelAgentManager > NULL);
 	assert(PerfParallelAgentManager != NULL);
-
 
 	Memory_manager* memory_manager = new Memory_manager(Size1, Size2, SFastMemory, SMainMemory, MainMemThroughput);
 	Manager_of_parallel_agents* manager_of_parallel_agents = new Manager_of_parallel_agents(k1, FastMemThroughput, PerfParallelAgentManager, MainMemThroughput);
 	Query_manager* query_manager = new Query_manager();
-/*TimeFirst - подготовительный шаг------------------------------------------------------------------*/
+/*-------------------------------TimeFirst - подготовительный шаг-----------------------------------*/
 	t_load = memory_manager->Ftml + memory_manager->FragMM + memory_manager->GDAfrag_t1 + manager_of_parallel_agents->GDAfrag_t2;
 	assert(t_load > NULL);
 #ifdef DEBUG 
@@ -170,7 +153,7 @@ int main()
 	cout << "t_0 " << t_0 << endl;
 #endif
 	TimeFirst = t_0 + t_load; 
-/*Processing_steps - Шаги обработки----------------------------------------------------------------*/
+/*--------------------------------Processing_steps - Шаги обработки--------------------------------*/
 	Time_j = manager_of_parallel_agents->MesAfrag + manager_of_parallel_agents->ProcRequest;
 	assert(Time_j > NULL);
 #ifdef DEBUG 
@@ -185,7 +168,7 @@ int main()
 #ifdef DEBUG 
 	cout << "N_ProcessingSteps " << N_ProcessingSteps << endl;
 #endif
-/*TimeFinal - Завершающий шаг ---------------------------------------------------------------------*/
+/*-------------------------------TimeFinal - Завершающий шаг---------------------------------------*/
 	cout << "Подсчет результатов  \n";
 	TALL = TimeFirst + N_ProcessingSteps + TimeFinal;
 	cout << TALL << " Условных единиц" << endl;
